4d48e38e1bfae20c33950bb2aa291c5ed5112c3f
Yan Braslavsky <yan.braslavsky@gmail.com>
2025-01-02 08:36:56 +0100

    update favicon to SVG format, adjust TypeScript configuration, and modify test case for button rendering

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders generate button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/generate/i);
+  expect(buttonElement).toBeInTheDocument();
 });
